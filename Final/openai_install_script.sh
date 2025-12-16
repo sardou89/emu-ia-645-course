5e1ea1049bc6d0f7c4606bc1e03ba3f3d9195071
sardou89 <ssabeyoy@emich.edu>
2025-12-15 23:02:05 -0500

    IA-645 Final: Fix OpenAI Docker API key injection

--- Final/openai_install_script.sh
+++ Final/openai_install_script.sh
@@ -1,7 +1,10 @@
 #!/bin/bash -x
 
 docker build -t openai-app .
+docker run -d \
+  -p 8001:8001 \
+  -e OPENAI_API_KEY=$OPENAI_API_KEY \
+  openai-app
 
-docker run -d -p 8001:8001 --env-file .env openai-app
 
 
